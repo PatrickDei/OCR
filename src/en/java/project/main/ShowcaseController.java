24164d32876a80afd8efd8a803e1f866c14cb7cd
Patrick Deisinger <patrick.deis@gmail.com>
2020-05-26 23:21:56 +0200

    Directory selection implemented

---
+++ src/en/java/project/main/ShowcaseController.java
@@ -0,0 +1,34 @@
+package en.java.project.main;
+
+import java.io.File;
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.DirectoryChooser;
+
+public class ShowcaseController {
+
+	/*@FXML
+	private TableView tab;*/
+	
+	public void chooseDirectory(){
+		
+		DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File("src"));
+        
+        try {
+        	File selectedDirectory = directoryChooser.showDialog(null);
+        	
+	        if(selectedDirectory.isDirectory()) {
+	        	System.out.println(selectedDirectory.getAbsolutePath());
+	        	BorderPane center = FXMLLoader.load(getClass().getResource("Showcase.fxml"));
+	        	Main.setCenterPane(center);
+	        }
+        }catch (IOException e) {
+	            e.printStackTrace();
+	    }catch(NullPointerException ex) {
+	    	System.out.println("No directory chosen");
+	    }
+	}
+}
